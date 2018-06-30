77d57e07f91a6cb889bdb8f8bde434c12c7aafbd
Simon Cropp <simon.cropp@gmail.com>
2018-06-30 19:55:12 +1000

    .

--- Bogus.NodaTime/NodaTimeDataSet.cs
+++ Bogus.NodaTime/NodaTimeDataSet.cs
@@ -1,3 +1,4 @@
+using System;
 using NodaTime;
 
 namespace Bogus.NodaTime
@@ -15,11 +16,39 @@ namespace Bogus.NodaTime
         }
 
         /// <summary>
+        /// Get a random <see cref="Period"/>. Default 1 week/7 days.
+        /// </summary>
+        public Period Period(Period maximum = null)
+        {
+            Period span;
+            if (maximum != null)
+            {
+                span = maximum;
+            }
+            else
+            {
+                span = global::NodaTime.Period.FromDays(7);
+            }
+
+            var partTimeSpanTicks = Random.Double() * span.ToDuration().TotalTicks;
+
+            return global::NodaTime.Period.FromTicks(Convert.ToInt64(partTimeSpanTicks));
+        }
+
+        /// <summary>
         /// Get a random <see cref="Duration"/>. Default 1 week/7 days.
         /// </summary>
         public Duration Duration(Duration? maximum = null)
         {
-            var span = maximum ?? global::NodaTime.Duration.FromDays(7);
+            Duration span;
+            if (maximum != null)
+            {
+                span = maximum.Value;
+            }
+            else
+            {
+                span = global::NodaTime.Duration.FromDays(7);
+            }
 
             var partTimeSpanTicks = Random.Double() * span.TotalTicks;
 
