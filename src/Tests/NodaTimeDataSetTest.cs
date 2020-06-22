bc2ce399b9dd48baebc12fd182ea470afdcb0608
SimonCropp <simon.cropp@gmail.com>
2020-06-22 21:17:40 +1000

    Update NodaTimeDataSetTest.cs

--- src/Tests/NodaTimeDataSetTest.cs
+++ src/Tests/NodaTimeDataSetTest.cs
@@ -17,14 +17,14 @@ public class NodaTimeDataSetTest
     [Fact]
     public void Period_with_large_max_period()
     {
-        var period = dataSet.Period(global::NodaTime.Period.FromDays(1000));
+        var period = dataSet.Period(NodaTime.Period.FromDays(1000));
         Assert.NotNull(period);
     }
 
     [Fact]
     public void Period_with_units()
     {
-        var period = dataSet.Period(global::NodaTime.Period.FromMonths(2), units: PeriodUnits.AllUnits);
+        var period = dataSet.Period(NodaTime.Period.FromMonths(2), units: PeriodUnits.AllUnits);
         Assert.NotNull(period);
     }
 
@@ -32,9 +32,9 @@ public class NodaTimeDataSetTest
     public void Period_with_anchor_date()
     {
         var anchorDateTime = new LocalDateTime(2013,1,3,4,6,7);
-        var period = dataSet.Period(global::NodaTime.Period.FromMonths(2), anchorDateTime);
+        var period = dataSet.Period(NodaTime.Period.FromMonths(2), anchorDateTime);
 
-        (anchorDateTime + period).Should().BeLessOrEqualTo(anchorDateTime + global::NodaTime.Period.FromMonths(2));
+        (anchorDateTime + period).Should().BeLessOrEqualTo(anchorDateTime + NodaTime.Period.FromMonths(2));
     }
 
 
