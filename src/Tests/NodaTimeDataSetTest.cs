c11ca222fe5cbba6e861730a6b54dd818fc0e0d6
Jonathan Dickinson <jcdickinson@users.noreply.github.com>
2023-07-15 05:00:02 -0400

    fix: ensure that Random flows correctly to child datasets (#443)
    Bogus has a mechanism to ensure that the value of Random correctly flows
    to child datasets, this commit adds the nessecary wiring for this to
    work.
    
    Co-authored-by: Jonathan Dickinson <gitkraken@jonathan.dickinson.id>

--- src/Tests/NodaTimeDataSetTest.cs
+++ src/Tests/NodaTimeDataSetTest.cs
@@ -1,4 +1,4 @@
-﻿using Bogus.NodaTime;
+using Bogus.NodaTime;
 using FluentAssertions;
 using NodaTime;
 
@@ -30,7 +30,7 @@ public class NodaTimeDataSetTest
     [Fact]
     public void Period_with_anchor_date()
     {
-        var anchorDateTime = new LocalDateTime(2013,1,3,4,6,7);
+        var anchorDateTime = new LocalDateTime(2013, 1, 3, 4, 6, 7);
         var period = dataSet.Period(NodaTime.Period.FromMonths(2), anchorDateTime);
 
         (anchorDateTime + period).Should().BeLessOrEqualTo(anchorDateTime + NodaTime.Period.FromMonths(2));
@@ -49,6 +49,20 @@ public class NodaTimeDataSetTest
     {
         var calendarSystem = dataSet.CalendarSystem();
         Assert.NotNull(calendarSystem);
+    }
+
+    [Fact]
+    public void Random_changed()
+    {
+        var newRandom = new Bogus.Randomizer();
+        var sut = new NodaTimeDataSet();
+        sut.Random = newRandom;
+
+        sut.Instant.Random.Should().BeSameAs(newRandom);
+        sut.LocalDate.Random.Should().BeSameAs(newRandom);
+        sut.LocalDateTime.Random.Should().BeSameAs(newRandom);
+        sut.LocalTime.Random.Should().BeSameAs(newRandom);
+        sut.ZonedDateTime.Random.Should().BeSameAs(newRandom);
     }
 
     public NodaTimeDataSetTest() =>
