9fed90880bf048344f905419161bdef2854fb7e1
Simon Cropp <simon.cropp@gmail.com>
2024-03-06 21:50:49 +1100

    refs

--- src/Tests/SeededTest.cs
+++ src/Tests/SeededTest.cs
@@ -6,10 +6,6 @@ public class SeededTest
     protected static void ResetGlobalSeed() =>
         Randomizer.Seed = new(3116);
 
-    protected IEnumerable<T> Make<T>(int times, Func<T> a) =>
-        Enumerable.Range(0, times)
-            .Select(_ => a()).ToArray();
-
     public SeededTest() =>
         //set the random gen manually to a seeded value
         ResetGlobalSeed();
