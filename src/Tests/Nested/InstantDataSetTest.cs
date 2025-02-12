cd5a78bc8d627d78143f5eb692dc9d99581cb3ae
Simon Cropp <simon.cropp@gmail.com>
2025-02-12 22:32:40 +1100

    cleanup

--- src/Tests/Nested/InstantDataSetTest.cs
+++ src/Tests/Nested/InstantDataSetTest.cs
@@ -1,43 +1,38 @@
 ﻿using Bogus.NodaTime;
-using FluentAssertions;
 using NodaTime;
+using Xunit;
 
-public class InstantDataSetTest :
-    SeededTest
+public class InstantDataSetTest : SeededTest
 {
-    public InstantDataSetTest() =>
-        dataSet = new();
+    public InstantDataSetTest() => dataSet = new();
 
     InstantDataSet dataSet;
 
     [Fact]
     public void Future()
     {
-        var starting = Instant.FromUtc(2015,6,6,4,17,41);
-        dataSet.Future(reference: starting).Should()
-            .BeLessOrEqualTo(starting.Plus(Duration.FromDays(100)))
-            .And
-            .BeGreaterOrEqualTo(starting);
+        var starting = Instant.FromUtc(2015, 6, 6, 4, 17, 41);
+        var result = dataSet.Future(reference: starting);
+        Assert.True(result <= starting.Plus(Duration.FromDays(100)));
+        Assert.True(result >= starting);
     }
 
     [Fact]
     public void Future_with_options()
     {
         var starting = Instant.FromUtc(2015, 6, 6, 4, 17, 41);
-        dataSet.Future(reference: starting, daysToGoForward: 500).Should()
-            .BeLessOrEqualTo(starting.Plus(Duration.FromDays(500)))
-            .And
-            .BeGreaterOrEqualTo(starting);
+        var result = dataSet.Future(reference: starting, daysToGoForward: 500);
+        Assert.True(result <= starting.Plus(Duration.FromDays(500)));
+        Assert.True(result >= starting);
     }
 
     [Fact]
     public void Past()
     {
         var starting = Instant.FromUtc(2015, 6, 6, 4, 17, 41);
-        dataSet.Past(reference: starting).Should()
-            .BeLessOrEqualTo(starting)
-            .And
-            .BeGreaterOrEqualTo(starting.Minus(Duration.FromDays(100)));
+        var result = dataSet.Past(reference: starting);
+        Assert.True(result <= starting);
+        Assert.True(result >= starting.Minus(Duration.FromDays(100)));
     }
 
     [Fact]
@@ -45,61 +40,50 @@ public class InstantDataSetTest :
     {
         var instant = dataSet.Past(0);
         var now = SystemClock.Instance.GetCurrentInstant();
-        instant.Should()
-            .BeLessOrEqualTo(now)
-            .And
-            .BeGreaterOrEqualTo(now.Minus(Duration.FromDays(1)));
+        Assert.True(instant <= now);
+        Assert.True(instant >= now.Minus(Duration.FromDays(1)));
     }
 
     [Fact]
     public void Past_with_custom_options()
     {
         var starting = Instant.FromUtc(2015, 6, 6, 4, 17, 41);
-        dataSet.Past(reference: starting, daysToGoBack: 500).Should()
-            .BeLessOrEqualTo(starting)
-            .And
-            .BeGreaterOrEqualTo(starting.Minus(Duration.FromDays(500)));
+        var result = dataSet.Past(reference: starting, daysToGoBack: 500);
+        Assert.True(result <= starting);
+        Assert.True(result >= starting.Minus(Duration.FromDays(500)));
     }
 
     [Fact]
     public void Recently()
     {
         var start = SystemClock.Instance.GetCurrentInstant();
-        dataSet.Recent()
-            .Should()
-            .BeLessOrEqualTo(start)
-            .And
-            .BeGreaterOrEqualTo(start.Minus(Duration.FromDays(1)));
+        var result = dataSet.Recent();
+        Assert.True(result <= start);
+        Assert.True(result >= start.Minus(Duration.FromDays(1)));
     }
-
 
     [Fact]
     public void Random_time_between_two_dates()
     {
-        var start= Instant.FromUtc(2015, 6, 6, 4, 17, 41);
+        var start = Instant.FromUtc(2015, 6, 6, 4, 17, 41);
         var end = Instant.FromUtc(2015, 7, 6, 4, 17, 41);
 
-        dataSet.Between(start, end)
-            .Should()
-            .BeGreaterOrEqualTo(start)
-            .And
-            .BeLessOrEqualTo(end);
+        var result = dataSet.Between(start, end);
+        Assert.True(result >= start);
+        Assert.True(result <= end);
 
-        //and reverse...
-        dataSet.Between(end, start)
-            .Should()
-            .BeGreaterOrEqualTo(start)
-            .And
-            .BeLessOrEqualTo(end);
+        // and reverse...
+        result = dataSet.Between(end, start);
+        Assert.True(result >= start);
+        Assert.True(result <= end);
     }
 
     [Fact]
     public void Time_that_will_happen_soon()
     {
         var start = SystemClock.Instance.GetCurrentInstant();
-        dataSet.Soon(3)
-            .Should()
-            .BeGreaterThan(start)
-            .And.BeLessThan(start.Plus(Duration.FromDays(3)));
+        var result = dataSet.Soon(3);
+        Assert.True(result > start);
+        Assert.True(result < start.Plus(Duration.FromDays(3)));
     }
 }
